cc6951696a1ff8b6d58ae294b86081354b99a01c
sagar2123 <33391765+sagar2123@users.noreply.github.com>
2024-03-04 11:07:32 +0530

    Update apiFactory.js

--- src/Service/apiFactory.js
+++ src/Service/apiFactory.js
@@ -3,3 +3,12 @@ import axios from "axios";
 export const getPageData = (pageId) => {
     return axios.get(`https://hn.algolia.com/api/v1/search?page=${pageId}`);
 }
+
+export const testSum = (a, b) => {
+    return a+b
+}
+
+//Adding to just see how the test coverage works, not required and can be removed later
+export const randomComparisonFunction = (a, b) => {
+    return a>b ? a : b;
+}
